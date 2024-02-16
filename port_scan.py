df32d5f7110d02375226fc5d7ae59bd4c3aedd55
Ashish <137613005+zebraxss48@users.noreply.github.com>
2024-02-16 23:16:51 +0530

    Add files via upload
    A port scanner is a software application designed to check open ports on a server's IP address or hostname by sending probe packets to TCP or UDP ports and checking the responses. Find specific details about each port scanning method below.
    
    This tool will help you to find open ports 
    
    made_by Ashish_Rai

---
+++ port_scan.py
@@ -0,0 +1,57 @@
+
+#!/usr/bin/python3
+
+import socket
+import sys
+import time
+import threading
+
+usage = "python3 port_scaning.py TARGET START_PORT END_PORT"
+
+print("_" *70)
+print("python Simple Port Scanner")
+print("_"*70)
+
+
+start_time = time.time()
+
+if(len(sys.argv) != 4):
+    print(usage)
+    sys.exit()
+
+
+try:
+    target = socket.gethostbyname(sys.argv[1])
+except socket.gaierror:
+    print("Name resolution error")
+    sys.exit()
+
+start_port = int(sys.argv[2])
+end_port = int(sys.argv[3])
+
+print("Scaning target", target)
+
+def scan_port(port):
+    # print("Scaning port:" , port)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2)
+    conn = s.connect_ex((target, port))
+    if (not conn):
+        print("port {} is OPEN".format(port))
+    s.close()
+
+
+for port in range(start_port , end_port+1):
+
+    thread = threading.Thread(target= scan_port, args= (port,))
+
+    thread.start()
+
+
+end_time = time.time()
+print("Time elapsed:", start_time + end_port)
+
+
+    
+
+
